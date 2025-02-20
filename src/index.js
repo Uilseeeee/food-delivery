bad69701b49209bf2e887c1506fa215115b198ee
Uilsee <uilseeu437@gmail.com>
2025-02-20 08:47:39 +0800

    main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { getUsers } from './resolvers/users/get-users.js';
 import { createUser } from './resolvers/users/create-users.js';
 import { deleteUser } from './resolvers/users/delete-users.js';
-// import { updateUser } from './resolvers/users/update-users.js';
+import { updateUser } from './resolvers/users/update-users.js';
 
 const app = express();
 const port = 3000;
@@ -18,7 +18,7 @@ app.get('/users', getUsers);
 
 app.delete('/users', deleteUser);
 
-// app.put ('/users', updateUser);
+app.put ('/users', updateUser);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- src/resolvers/users/update-users.js
+++ src/resolvers/users/update-users.js
@@ -1,12 +1,12 @@
+import fs from "fs";
 
-// export const updateUser = (req, res) => {
-// if (user.length > 0) {
-//     user[0] = { id: 3, name: 'Updated' };
-//     res.json(user[0]);
-
-// }
-// else {
-//     res.json({ message: "No user to update" });
-// }
-
-// }
+export const updateUser = (req, res) => {
+  const rawUsers = fs.readFileSync("src/db/users.json");
+    const user = JSON.parse(rawUsers);
+  if (user.length > 0) {
+    user[0] = { id: 4, name: "Updated" };
+    res.json(user[0]);
+  } else {
+    res.json({ message: "No user to update" });
+  }
+};
