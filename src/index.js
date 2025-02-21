46f2840d21299920771beaca6720a159e85e3d6e
Uilsee <uilseeu437@gmail.com>
2025-02-21 12:29:15 +0800

    so

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import express from "express";
 import { userRouter } from "./routes/users-router.js";
 import { moviesRouter } from "./routes/movie-router.js";
+import cors from "cors";
 
 
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/users", userRouter);
 
 app.use("/movies", moviesRouter);
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/resolvers/movies/create-movies.js
+++ src/resolvers/movies/create-movies.js
@@ -2,8 +2,13 @@ import fs from "fs"
 
 export const createMovies = (req, res) => {
     const rawMovies = fs.readFileSync('src/db/movies.json');
+    const {title , genres , year , rating} = req.body;
+    if (!title || !genres || !year || !rating) {
+        return res.send({ message: "All fields are required" });
+    }
+   
     const movies = JSON.parse(rawMovies);
-    movies.push(req.body);
-    res.send(req.body);
+    movies.push({title , genres , year , rating});  
     fs.writeFileSync('src/db/movies.json', JSON.stringify(movies));
+    res.send("Successfully added movie");
 }
